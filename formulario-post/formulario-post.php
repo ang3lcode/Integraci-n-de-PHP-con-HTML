e20718423d34e84bae478c3189b51abf6a41da12
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-09-25 18:32:38 -0500

    formulario de post

---
+++ formulario-post/formulario-post.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>formulario por post</title>
+</head>
+<body>
+    <form action="./server.php" method="post">
+        <label for="nombre">nombre</label>
+        <input type="text" name="nombre" id="nombre">
+        <br>
+
+        <label for="edad">edad</label>
+        <input type="text" name="edad" id="edad">
+
+        <button type="submit">formulario</button>
+    </form>
+</body>
+</html>
