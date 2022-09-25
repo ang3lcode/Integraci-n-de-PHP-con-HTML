286805770a984c991fc8a10d02a39c3c995b5599
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-09-25 14:06:19 -0500

    primeras clases de php con html

---
+++ php-preprocesador.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>¡html en php!</title>
+</head>
+<body>
+    <h1>¡hola desde php!</h1>
+
+    <?php echo "<p> Ahora esto se esta imprimiendo con PHP  </p>";
+    ?>
+    
+    
+</body>
+</html>
